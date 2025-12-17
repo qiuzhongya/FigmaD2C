a024fb06a73d6eb16e3a2279bd8c65305add7dda
qiuzhongya <31063746+qiuzhongya@users.noreply.github.com>
2025-12-17 10:45:54 +0800

    Fix print statement to include candidate_task_url

--- FigmaTranslate.py
+++ FigmaTranslate.py
@@ -228,7 +228,7 @@ def sechdule_task(figma_tasks):
         if time_difference.total_seconds() / 60 < 3:
              clamdown = False
     print(f"running task: {running_task_cnt}, prepare task: {remain_task_cnt}, already failed task: {failed_task},"
-          f"is clamdown time: {clamdown}, waiting figma: {candidate_task_url}")
+          f"is clamdown time: {clamdown}, waiting figma: {candidate_task_url},")
     if running_task_cnt < 5 and remain_task_cnt > 0 and clamdown and candidate_task_url:
         create_success, create_result = create_task(candidate_task_url, figma_tasks[candidate_task_url][2], USER_NAME)
         if create_success:
